19cb58270f887efe33bcd2c7ff9946597cabef1a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-22 20:28:59 +0100

    fixed code, comment line was uncommented

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express'), app = express(), cors = require('cors'),
-      bodyParser = require('body-parser');
+const express = require("express"),
+  app = express(),
+  cors = require("cors"),
+  bodyParser = require("body-parser");
 
-make server object that contain port property and the value for our server.
+// make server object that contain port property and the value for our server.
 var server = {
-   port: 4040
-   };
+  port: 4040,
+};
 
 // use the modules
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 // starting the server
-app.listen(server.port,
-           () => console.log(`Server started, listening port: ${server.port}`));
+app.listen(server.port, () =>
+  console.log(`Server started, listening port: ${server.port}`)
+);
