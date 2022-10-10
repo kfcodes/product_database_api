f8b1fcbe3e79feac9a620d183d6d1127dff9f215
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-10 13:05:54 +0100

    Modified products routes to select with WHERE clause

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,20 @@
 const express = require("express"),
   router = express.Router();
 
+// let sql = `SELECT * FROM product_database`;
+// let sql = `SELECT * FROM product_database WHERE product_id LIKE (?)`;
 // get product lists
 router.get("/list", function (req, res) {
-  let sql = `SELECT * FROM product`;
+  let sql = `SELECT * FROM product_database WHERE product_id LIKE "zu%"`;
+  let value = req.body.prefix;
+  console.log(value);
+  // db.query(sql, [ value ], function(err, data, fields) {
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.json({
       status: 200,
       data,
+      value,
       message: "Product lists retrieved successfully",
     });
   });
@@ -16,8 +22,8 @@ router.get("/list", function (req, res) {
 
 // create new product
 router.post("/new", function (req, res) {
-  let sql = `INSERT INTO product(name, type) VALUES (?)`;
-  let values = [req.body.name, req.body.gender];
+  let sql = `INSERT INTO product_database(name, type) VALUES (?)`;
+  let values = [req.body.name, req.body.type];
   db.query(sql, [values], function (err, data, fields) {
     if (err) throw err;
     res.json({ status: 200, message: "New product added successfully" });
