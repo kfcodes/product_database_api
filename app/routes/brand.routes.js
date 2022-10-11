aeda87a198594500799b8cbcf2be107e9d5dc990
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-11 18:40:55 +0100

    Updated structure of routes for brands

--- app/routes/brand.routes.js
+++ app/routes/brand.routes.js
@@ -1,27 +1,6 @@
-const express = require("express"),
-  router = express.Router();
+module.exports = (app) => {
+  const brands = require("../controllers/employee.controller.js");
 
-// get brand lists
-router.get("/list", function (req, res) {
-  let sql = `SELECT * FROM brand`;
-  db.query(sql, function (err, data, fields) {
-    if (err) throw err;
-    res.json({
-      status: 200,
-      data,
-      message: "Brand lists retrieved successfully",
-    });
-  });
-});
-
-// create new brand
-router.post("/new", function (req, res) {
-  let sql = `INSERT INTO brand(name) VALUES (?)`;
-  let values = [req.body.name];
-  db.query(sql, [values], function (err, data, fields) {
-    if (err) throw err;
-    res.json({ status: 200, message: "New brand added successfully" });
-  });
-});
-
-module.exports = router;
+  // Retrieve all brands
+  app.get("/brands", brands.findAll);
+};
