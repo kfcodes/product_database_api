f9aa637cefa07354f16648608d234e705b66cc28
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-18 18:51:17 +0100

    added routes, controller and model files to get all prducts from db

---
+++ app/controllers/product.controller.js
@@ -0,0 +1,14 @@
+// Get all products from the db
+const Product = require("../models/products.model.js");
+
+// Retrieve all products from the database.
+exports.findAll = (req, res) => {
+  Product.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving products.",
+      });
+    else res.send(data);
+  });
+};
---
+++ app/models/products.model.js
@@ -0,0 +1,16 @@
+const sql = require("./db.js");
+
+Product.getAll = (result) => {
+  sql.query("SELECT * FROM product", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("products: ", res);
+    result(null, res);
+  });
+};
+
+module.exports = Product;
