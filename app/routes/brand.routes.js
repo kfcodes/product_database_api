5818a92c4bac4e905a3e283666bc3c295d02dff9
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-23 11:04:14 +0100

    Updated the brand routes to follow Model and Controller convention

--- app/routes/brand.routes.js
+++ app/routes/brand.routes.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const brands = require("../controllers/brand.controller.js");
 
 // Create a new brand
-router.app.post("/", brands.create);
+router.post("/", brands.create);
 
 // Retrieve all brands
-router.app.get("/", brands.findAll);
+router.get("/", brands.findAll);
 
 // Retrieve a single brand with brandId
-router.app.get("/:brandId", brands.findOne);
+router.get("/:brandId", brands.findOne);
 
 // Update a brand with brandId
-router.app.put("/brandId", brands.update);
+router.put("/brandId", brands.update);
 
 // Delete a brand with brandId
-router.app.delete("/brandId", brands.delete);
+router.delete("/brandId", brands.delete);
 
 // Delete all brands
-router.app.delete("/", brands.deleteAll);
+router.delete("/", brands.deleteAll);
 
 module.exports = router;
