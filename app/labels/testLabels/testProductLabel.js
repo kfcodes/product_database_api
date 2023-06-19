d9efd00fe47a359b2a787f51130c13438e1f5130
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-19 15:59:58 +0100

    Created a module to test label with data

---
+++ app/labels/testLabels/testProductLabel.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+function newLoop() {
+  fs.writeFile("testproduct.zpl", "", function () {
+    console.log("Empty File created");
+  })
+    
+      writeToLabel("outline").then(
+      writeToLabel("data").then(
+      writeToLabel("end")))
+  .catch(err => console.log(err))
+}
+
+module.exports.CreateLabel = async function () {
+   newLoop()
+  // printLabel();
+};
+
+function writeToLabel(pallet) {
+  fs.appendFile("testproduct.zpl", pallet, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
+}
