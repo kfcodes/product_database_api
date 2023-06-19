b134343afb7e8129a2c7bee605b74ba0757c94e0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-19 16:55:21 +0100

    Created a module to take the values and locations for each product on the pallet

---
+++ app/labels/label_code/palletLabel/palletProduct.js
@@ -0,0 +1,31 @@
+function emptyPalletProductData(location) {
+  let product_data = `
+^FO${location - 30},10^GB160,1170,5^FS
+^FO${location + 50},10^GB5,1170,5^FS
+^FO${location},20^A0,40^FDLOT:^FS
+^FO${location},300^A0,40^FDBBE:^FS
+^FO${location},520^A0,40^FDBATCH:^FS
+^FO${location},920^A0,40^FDQTY:^FS
+^XZ
+`;
+  return product_data;
+}
+
+function blankPalletProductData(pallet, location) {
+  let product_data = `
+^FO${location - 30},10^GB160,1170,5^FS
+^FO${location + 50},10^GB5,1170,5^FS
+^FO${location + 70},50^A0,37^FD
+${pallet.product_description.toUpperCase()}
+^FS
+^FO${location},20^A0,40^FDLOT:^FS
+^FO${location},120^A0,35^FD${pallet.lot}^FS
+^FO${location},300^A0,40^FDBBE:^FS
+^FO${location},400^A0,35^FD${pallet.bbe}^FS
+^FO${location},520^A0,40^FDBATCH:^FS
+^FO${location},650^A0,35^FD${pallet.batch}^FS
+^FO${location},920^A0,40^FDQTY:^FS
+^FO${location},1050^A0,35^FD${pallet.quantity}^FS
+`;
+  return product_data;
+}
