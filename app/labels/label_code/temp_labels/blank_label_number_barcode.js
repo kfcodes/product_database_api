159cd3599d85eef86c1b72fc6606c3df059c71ee
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-19 17:48:45 +0100

    Created a module to print a series of labels between two numbers

---
+++ app/labels/label_code/temp_labels/blank_label_number_barcode.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+let startNumber = ;
+let finishNumber = ;
+
+// create a stream
+const stream = fs.createWriteStream('empty_label.zpl', { flags: 'a' })
+
+for (let number = startNumber; number < finishNumber+1; number++) {
+  stream.write(`^XA
+^FX Code 128 Barcode That will be in the top left of the label this will encode the id of the pallet
+^FO580,50
+^BCR,180,Y,N,N,N,N
+^FV
+${number}
+^FS
+^FX This puts the pallet Number at the top of the label
+^FO720,400
+^A0R,80,80
+^FV
+Pallet No. ${number}
+^FS
+^MCN^XZ`)
+}
+
+stream.write(`^MCY^XZ`)
+stream.end()
