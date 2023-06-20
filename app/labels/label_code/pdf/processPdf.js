686a5efab6851b6f9c8fa943276587e7ced9940c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-20 11:35:45 +0100

    Created a module to split and print pdf files

---
+++ app/labels/label_code/pdf/processPdf.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+
+module.exports.PrintPdf = async function (pdfname, result) {
+  console.log("The File was Split");
+  console.log(splitfile);
+  result(null);
+  return;
+};
+
+
+
