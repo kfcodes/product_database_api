c09c969dd67e5344ec1fdc1a9ac721f948bfe789
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-20 11:37:33 +0100

    Created a function to split pdf's in half

--- app/labels/label_code/pdf/processPdf.js
+++ app/labels/label_code/pdf/processPdf.js
@@ -2,11 +2,29 @@ const fs = require("fs");
 const { exec } = require("child_process");
 
 module.exports.PrintPdf = async function (pdfname, result) {
+  const splitfile = splitPdf(pdfname);
   console.log("The File was Split");
   console.log(splitfile);
+  printFile(splitfile);
   result(null);
   return;
 };
 
+function splitPdf(pdf) {
+  exec(
+    `mutool poster -x 2 uploads/${pdf} uploads/${pdf}-split.pdf`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+    }
+  );
+        return `${pdf}-split`
+}
 
 
