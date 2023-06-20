901d098b848180fb5a3356b7cd07017fb9bfc480
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-20 14:35:19 +0100

    Created module to load the hazard label data and append it to label

---
+++ app/labels/jsFiles/internal/hazards/hazardsData.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+function hazardsLabel(data) {
+`
+^FX HAZARDS LIST
+^FO333,20^A0,25^FD${HAZARD_}^FS
+^FO293,20^A0,25^FD${HAZARD_1}^FS
+^FO253,20^A0,25^FD${HAZARD_2}^FS
+^FO213,20^A0,25^FD${HAZARD_3}^FS
+^FO173,20^A0,25^FD${HAZARD_4}^FS
+^FO133,20^A0,25^FD${HAZARD_5}^FS
+^FO93,20^A0,25^FD${HAZARD_6}^FS
+^FO53,20^A0,25^FD${HAZARD_7}^FS
+^FO13,20^A0,25^FD${HAZARD_8}^FS
+^FO333,620^A0,25^FD${HAZARD_9}^FS
+^FO293,620^A0,25^FD${HAZARD_10}^FS
+^FO253,620^A0,25^FD${HAZARD_11}^FS
+^FO213,620^A0,25^FD${HAZARD_12}^FS
+^FO173,620^A0,25^FD${HAZARD_13}^FS
+^FO133,620^A0,25^FD${HAZARD_14}^FS
+^FO93,620^A0,25^FD${HAZARD_15}^FS
+^FO53,620^A0,25^FD${HAZARD_16}^FS
+
+^FX PRODUCT INFORMATION
+^FO400,330^A0,30^FD${data.precautions}^FS
+^FO675,325^A0,30^FD${data.code}^FS
+^FO625,325^A0,30^FD${data.name}^FS
+^FO575,325^A0,30^FD${data.lot}^FS
+^FO525,325^A0,30^FD${data.bbe}^FS
+^FO475,325^A0,30^FD${data.date}^FS
+  `;
+}
+
+function newLoop() {
+  fs.writeFile("testHazards.zpl", "", function () {
+    console.log("Empty File created");
+  })
+    .then(
+      writeToLabel("outline").then(
+        writeToLabel("data").then(writeToLabel("end"))
+      )
+    )
+    .catch((err) => console.log(err));
+}
+
+module.exports.CreateLabel = async function (data) {
+hazardsLabel(data);
+};
+
+function writeToLabel(pallet) {
+  fs.appendFile("hazards.zpl", hazards, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
+}
