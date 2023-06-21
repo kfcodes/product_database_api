7b10793ed450a7e21fa92ee854e3de694498947c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-21 12:18:17 +0100

    Moved functions to append to file to dedicated module

--- app/labels/jsFiles/product/index.js
+++ app/labels/jsFiles/product/index.js
@@ -1,24 +1,9 @@
 const fs = require("fs");
 
-function newLoop() {
-  fs.writeFile("testproduct.zpl", "", function () {
-    console.log("Empty File created");
-  }).then(
+module.exports.CreateLabel = async function (data) {
       writeToLabel("outline").then(
       writeToLabel("data").then(
-      writeToLabel("end"))))
+      writeToLabel("end")))
   .catch(err => console.log(err))
 }
 
-module.exports.CreateLabel = async function () {
-   newLoop()
-};
-
-function writeToLabel(pallet) {
-  fs.appendFile("product.zpl", pallet, (err) => {
-    if (err) console.log(err);
-    else {
-      console.log("File written successfully\n");
-    }
-  });
-}
--- app/labels/jsFiles/writeToFile/write.js
+++ app/labels/jsFiles/writeToFile/write.js
@@ -1,14 +1,12 @@
+const fs = require("fs");
+
+function newLoop() {
+  fs.writeFile("testproduct.zpl", "", function () {
+    console.log("Empty File created");
+  }).then(
+
 module.exports.writeToFile = async function (file, data) {
   fs.appendFile(`${file}.zpl`, data, (err) => {
-    if (err) console.log(err);
-    else {
-      console.log("File written successfully\n");
-    }
-  });
-}
-
-function writeToLabel(pallet) {
-  fs.appendFile("product.zpl", pallet, (err) => {
     if (err) console.log(err);
     else {
       console.log("File written successfully\n");
