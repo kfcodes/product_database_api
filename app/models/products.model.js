ac18b3fa3f49c36b00e02c33c2f408c4cbb62c55
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-06-30 11:44:32 +0100

    Created a constructor for the pallet data

--- app/models/products.model.js
+++ app/models/products.model.js
@@ -16,6 +16,16 @@ const Po = function (po) {
   this.process.env.PO_2 = po.process.env.PO_2;
 };
 
+// Pallet Constructor;
+const Pallet = function (pallet) {
+  this.PALLET_1 = pallet.PALLET_1;
+  this.PALLET_2 = pallet.PALLET_2;
+  this.PALLET_3 = pallet.PALLET_3;
+  this.PALLET_4 = pallet.PALLET_4;
+  this.PALLET_5 = pallet.PALLET_5;
+  this.PALLET_6 = pallet.PALLET_6;
+};
+
 
 Product.getAll = (result) => {
   sql.query(
