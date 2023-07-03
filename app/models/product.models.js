9d2a89f47efd97b0fbd527c6c6109a82b9778816
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-03 12:55:04 +0100

    Moved variable names to .env file allowing for easier transfer to production

--- app/models/product.models.js
+++ app/models/product.models.js
@@ -8,7 +8,7 @@ const Product = function (product) {
 
 Product.getAll = (result) => {
   sql.query(
-    "SELECT product_id, product_description FROM product_database",
+    `SELECT ${process.env.PRODUCT_1}, ${process.env.PRODUCT_2} FROM ${process.env.PD}`,
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -21,25 +21,11 @@ Product.getAll = (result) => {
   );
 };
 
-Product.getAllFinishedProducts = (result) => {
-  sql.query(
-    "SELECT product_id, product_description FROM product_database where product_id rlike '^[a-z]'  AND product_description NOT LIKE 'do not use%';",
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      console.log("products: ", res);
-      result(null, res);
-    }
-  );
-};
 
-Product.findById = (productId, result) => {
+Product.findById = (pid, result) => {
   sql.query(
-    "SELECT * FROM product_info WHERE product_id = ?",
-    [productId],
+    `SELECT * FROM ${process.env.PI} WHERE ${process.env.1} = ?`,
+    [pid],
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -51,14 +37,13 @@ Product.findById = (productId, result) => {
         result(null, res[0]);
         return;
       }
-      // not found product with the id
       result({ kind: "not_found" }, null);
     }
   );
 };
 
 Product.getAllBrands = (result) => {
-  sql.query("SELECT * FROM brand", (err, res) => {
+  sql.query(`SELECT * FROM ${process.env.B}`, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);
@@ -69,9 +54,9 @@ Product.getAllBrands = (result) => {
   });
 };
 
-Product.productsFromBrand = (brandId, result) => {
+Product.productsFromBrand = (bid, result) => {
   sql.query(
-    `SELECT * FROM product_database WHERE product_id LIKE "${brandId}%"`,
+    `SELECT * FROM ${process.env.PD} WHERE ${process.env.1} LIKE "${bid}%`,
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -83,10 +68,10 @@ Product.productsFromBrand = (brandId, result) => {
     }
   );
 };
-Product.findBomById = (productId, result) => {
+Product.findBomById = (id, result) => {
   sql.query(
-    "SELECT * FROM bom_component_data WHERE bom_product_id= ?",
-    [productId],
+    `SELECT * FROM ${process.env.BCD} WHERE ${Pprocess.env.PRODUCT_5}= ?`,
+    [id],
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -98,16 +83,15 @@ Product.findBomById = (productId, result) => {
         result(null, res);
         return;
       }
-      // not found product with the id
       result({ kind: "not_found" }, null);
     }
   );
 };
 
-Product.findPalletById = (palletid, result) => {
+Product.findPalletById = (id, result) => {
   sql.query(
-    "SELECT * FROM pallet_info WHERE pallet_id= ?",
-    [palletid],
+    `SELECT * FROM ${process.env.PAI} WHERE ${process.env.PALLET1}= ?`,
+    [id],
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -124,9 +108,9 @@ Product.findPalletById = (palletid, result) => {
   );
 };
 
-Product.findPalletByBrand = (brandId, result) => {
+Product.findPalletByBrand = (id, result) => {
   sql.query(
-    `SELECT * FROM full_pallet_info WHERE brand_id LIKE "${brandId}%"`,
+    `SELECT * FROM ${process.env.FPAI} WHERE ${process.env.BID} LIKE "${id}%"`,
     (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -138,176 +122,4 @@ Product.findPalletByBrand = (brandId, result) => {
     }
   );
 };
-
-Product.printPalletLabel = (palletid, result) => {
-  sql.query(
-    "SELECT * FROM full_pallet_info WHERE pallet_id= ? AND pallet_item_product_id IS NOT null;",
-    [palletid],
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      if (res.length) {
-        result(null, res);
-        return;
-      }
-      result(null);
-      return;
-    }
-  );
-};
-
-Product.getRecentPallets = (result) => {
-  sql.query(
-    "SELECT * FROM test_view_2 ORDER by pallet_id Desc LIMIT 20",
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
-Product.newGetRecentPallets = (result) => {
-  sql.query(
-    "SELECT * FROM test_view_2 ORDER by pallet_id Desc LIMIT 20",
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
-Product.getLatestPallets = (result) => {
-  sql.query("SELECT * FROM more_pallet_info ORDER BY pallet_id DESC LIMIT 20 ", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    result(null, res);
-    return res;
-  });
-};
-
-Product.getLatestPalletProducts = async (palletid, result) => {
-  sql.query(
-    "SELECT * FROM test_view WHERE pallet_item_pallet_id = ? AND pallet_item_product_id IS NOT NULL",
-    [palletid],
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
-Product.getAllPallets = (result) => {
-  sql.query("select * from test_view_2", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    console.log("These are the pallets in the db: ", res);
-    result(null, res);
-  });
-};
-
-Product.getAllPalletItems = (result) => {
-  sql.query("SELECT * FROM test_view", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-    console.log("These are the pallet itemes in the db: ", res);
-    result(null, res);
-  });
-};
-
-Product.getPalletsForPallet = (palletid, result) => {
-  sql.query(
-    `SELECT * FROM test_view where pallet_item_pallet_id="${palletid}"`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      console.log("Products on the pallet: ", res);
-      result(null, res);
-    }
-  );
-};
-
-Product.getPalletsFromBrand = (ubid, result) => {
-  sql.query(
-    `SELECT * FROM full_pallet_info WHERE pallet_item_product_id LIKE "${ubid}%"`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      console.log("products: ", res);
-      result(null, res);
-    }
-  );
-};
-
-Product.getPalletData = (pid, result) => {
-  sql.query(
-    `select * from packing_list_view WHERE ID IS NOT NULL AND PALLET>${pid} ORDER BY substring(ID, 1, 2), PALLET, COMPANY, item_id DESC`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
-Product.getLatestData = (result) => {
-  sql.query(
-    `select ${process.env.1}, ${process.env.2}, ${process.env.4}, ${process.env.5}, ${process.env.6}, ${process.env.12}, ${process.env.13}, ${process.env.8}, ${process.env.9} from packing_list_view WHERE ${process.env.9} IN (select * from latest_pallets) AND ${process.env.2} IS NOT NULL ORDER BY ${process.env.9} DESC`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
-Product.getPackingListData = (pid, result) => {
-  sql.query(
-    `select ${process.env.1}, ${process.env.2}, ${process.env.3}, ${process.env.4}, ${process.env.5}, ${process.env.6}, ${process.env.7}, ${process.env.8}, ${process.env.9} from packing_list_view WHERE ${process.env.2} IS NOT NULL AND ${process.env.9}>${pid} ORDER BY substring(${process.env.2}, 1, 2), ${process.env.9}, ${process.env.14}, ${process.env.11} DESC`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      result(null, res);
-    }
-  );
-};
-
 
