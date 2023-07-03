a2e7b3b5c0ef792e90de227a142b0df0c664b0ad
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-03 14:26:39 +0100

    Added functions to find and create eol data in DB

--- app/models/eol.models.js
+++ app/models/eol.models.js
@@ -9,5 +9,35 @@ const Eol = function (eol) {
   this.EOL_5 = eol.EOL_5;
 };
 
+Eol.findEolById = (id, result) => {
+  sql.query(
+    `SELECT * FROM ${process.env.ET} WHERE ${process.env.EOL_6}= ?`,
+    [id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      if (res.length) {
+        console.log("found Eol details: ", res);
+        result(null, res);
+        return;
+      }
+      result({ kind: "not_found" }, null);
+    }
+  );
+};
+
+Eol.createNewEol = (Eol, result) => {
+  sql.query(`INSERT INTO ${process.env.ET} SET ?`, Eol, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+  });
+};
+
 
 module.exports = { Eol };
