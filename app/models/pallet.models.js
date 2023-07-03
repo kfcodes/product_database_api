65931a9a62f8f9719a9240f2f696ed29b415ab34
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-03 15:22:36 +0100

    Moved pallet models to their own module

---
+++ app/models/pallet.models.js
@@ -0,0 +1,13 @@
+const sql = require("./db.js");
+require('dotenv').config();
+
+// Pallet Constructor;
+const Pallet = function (pallet) {
+  this.PALLET_1 = pallet.PALLET_1;
+  this.PALLET_2 = pallet.PALLET_2;
+  this.PALLET_3 = pallet.PALLET_3;
+  this.PALLET_4 = pallet.PALLET_4;
+  this.PALLET_5 = pallet.PALLET_5;
+  this.PALLET_6 = pallet.PALLET_6;
+};
+
