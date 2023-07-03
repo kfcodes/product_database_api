c532820f98369d7d22ec30dba5bc52dd17548097
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-03 11:20:12 +0100

    Moved the remaining product functions to product module

--- app/models/product.models.js
+++ app/models/product.models.js
@@ -123,3 +123,147 @@ Product.findPalletById = (palletid, result) => {
     }
   );
 };
+
+Product.findPalletByBrand = (brandId, result) => {
+  sql.query(
+    `SELECT * FROM full_pallet_info WHERE brand_id LIKE "${brandId}%"`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      console.log("Pallets: ", res);
+      result(null, res);
+    }
+  );
+};
+
+Product.printPalletLabel = (palletid, result) => {
+  sql.query(
+    "SELECT * FROM full_pallet_info WHERE pallet_id= ? AND pallet_item_product_id IS NOT null;",
+    [palletid],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      if (res.length) {
+        result(null, res);
+        return;
+      }
+      result(null);
+      return;
+    }
+  );
+};
+
+Product.getRecentPallets = (result) => {
+  sql.query(
+    "SELECT * FROM test_view_2 ORDER by pallet_id Desc LIMIT 20",
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    }
+  );
+};
+
+Product.newGetRecentPallets = (result) => {
+  sql.query(
+    "SELECT * FROM test_view_2 ORDER by pallet_id Desc LIMIT 20",
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    }
+  );
+};
+
+Product.getLatestPallets = (result) => {
+  sql.query("SELECT * FROM more_pallet_info ORDER BY pallet_id DESC LIMIT 20 ", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    result(null, res);
+    return res;
+  });
+};
+
+Product.getLatestPalletProducts = async (palletid, result) => {
+  sql.query(
+    "SELECT * FROM test_view WHERE pallet_item_pallet_id = ? AND pallet_item_product_id IS NOT NULL",
+    [palletid],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    }
+  );
+};
+
+Product.getAllPallets = (result) => {
+  sql.query("select * from test_view_2", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    console.log("These are the pallets in the db: ", res);
+    result(null, res);
+  });
+};
+
+Product.getAllPalletItems = (result) => {
+  sql.query("SELECT * FROM test_view", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    console.log("These are the pallet itemes in the db: ", res);
+    result(null, res);
+  });
+};
+
+Product.getPalletsForPallet = (palletid, result) => {
+  sql.query(
+    `SELECT * FROM test_view where pallet_item_pallet_id="${palletid}"`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      console.log("Products on the pallet: ", res);
+      result(null, res);
+    }
+  );
+};
+
+Product.getPalletsFromBrand = (ubid, result) => {
+  sql.query(
+    `SELECT * FROM full_pallet_info WHERE pallet_item_product_id LIKE "${ubid}%"`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      console.log("products: ", res);
+      result(null, res);
+    }
+  );
+};
