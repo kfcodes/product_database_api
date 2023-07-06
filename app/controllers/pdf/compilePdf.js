812fa669f3368e5641311dbfbd09cd85eaa941f8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-06 16:10:23 +0100

    Modified the exports to use commonjs style

--- app/controllers/pdf/compilePdf.js
+++ app/controllers/pdf/compilePdf.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const hbs = require("handlebars");
 const path = require("path");
 
-export default async function compile(templateName, data) {
+async function compile(templateName, data) {
   const filePath = path.join(
     process.cwd(),
     "/app/pdf/templates",
@@ -11,3 +11,5 @@ export default async function compile(templateName, data) {
   const html = await fs.readFile(filePath, "utf-8");
   return hbs.compile(html)(data);
 };
+
+module.exports = compile;
