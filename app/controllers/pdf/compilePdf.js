20b85687b198a1bc545545682e9f7d6c8b311a90
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-06 15:51:14 +0100

    Moved the Compile pdf function to module

---
+++ app/controllers/pdf/compilePdf.js
@@ -0,0 +1,15 @@
+const fs = require("fs-extra");
+const hbs = require("handlebars");
+const path = require("path");
+
+const compile = async function (templateName, data) {
+  const filePath = path.join(
+    process.cwd(),
+    "/app/pdf/templates",
+    `${templateName}.hbs`
+  );
+  const html = await fs.readFile(filePath, "utf-8");
+  return hbs.compile(html)(data);
+};
+
+module.exports = compile;
