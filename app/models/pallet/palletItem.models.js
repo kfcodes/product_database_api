4d98d5fd2d79533f6246be6a7b499e4ff61455ec
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-07 15:19:25 +0100

    Moved pallet item models to pallet directory

--- app/models/pallet/palletItem.models.js
+++ app/models/pallet/palletItem.models.js
@@ -1,19 +1,18 @@
 const sql = require("./db.js");
 require('dotenv').config();
 
-//Pallet Item Constructor;
 const PalletItem = function (palletItem) {
-  this.PALLETITEM_1 = palletItem.PALLETITEM_1;
-  this.PALLETITEM_2 = palletItem.PALLETITEM_2;
-  this.PALLETITEM_3 = palletItem.PALLETITEM_3;
-  this.PALLETITEM_4 = palletItem.PALLETITEM_4;
-  this.PALLETITEM_4 = palletItem.PALLETITEM_4;
-  this.PALLETITEM_4 = palletItem.PALLETITEM_4;
-  this.PALLETITEM_4 = palletItem.PALLETITEM_4;
+  this.item_id = palletItem.item_id;
+  this.pallet_item_pallet_id = palletItem.pallet_item_pallet_id;
+  this.pallet_item_product_id = palletItem.pallet_item_product_id;
+  this.lot = palletItem.lot;
+  this.bbe = palletItem.bbe;
+  this.batch = palletItem.batch;
+  this.quantity = palletItem.quantity;
 };
 
 PalletItem.createNewPalletItem = (newItem, result) => {
-  sql.query(`INSERT INTO ${process.env.PIT} SET ?`, newItem, (err, res) => {
+  sql.query(`INSERT INTO pallet_item SET ?`, newItem, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -41,31 +40,5 @@ PalletItem.remove = (id, result) => {
   });
 };
 
-PalletItem.updatePalletItemById = (id, pallet_item, result) => {
-  sql.query(
-    `UPDATE pallet_item SET 
-    ${process.env.PALLETITEM_1}=${pallet_item.${process.env.PALLETITEM_1},
-    ${process.env.PALLETITEM_2}=${pallet_item.${process.env.PALLETITEM_2},
-    ${process.env.PALLETITEM_3}="${pallet_item.${process.env.PALLETITEM_3}",
-    ${process.env.PALLETITEM_4}="${pallet_item.${process.env.PALLETITEM_4}",
-    ${process.env.PALLETITEM_5}="${pallet_item.${process.env.PALLETITEM_5}",
-    ${process.env.PALLETITEM_6}="${pallet_item.${process.env.PALLETITEM_6}",
-    ${process.env.PALLETITEM_7}=${pallet_item.${process.env.PALLETITEM_7}
-    WHERE ${process.env.PALLETITEM_1}=${id}`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      if (res.affectedRows == 0) {
-        result({ kind: "not_found" }, null);
-        return;
-      }
-      console.log("updated pallet Item: ", { ...pallet_item });
-      result(null, { ...pallet_item });
-    }
-  );
-};
 
 module.exports = PalletItem;
