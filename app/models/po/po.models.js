713c1ed5b37c07a386ce2409fb455aebabe9ab0f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-07 15:06:14 +0100

    Moved model files to their own directories

--- app/models/po/po.models.js
+++ app/models/po/po.models.js
@@ -1,12 +1,12 @@
-const sql = require("./db.js");
+const {dbConnect} = require("./dbConnect.js");
 
-const Po = function (po) {
+const Po = async function (po) {
   this.po_id = po.po_id;
   this.customer_po_id = po.customer_po_id;
 };
 
 Po.createNewPo = async (po, result) => {
-  sql.query("INSERT INTO po SET ?", po, (err, res) => {
+  dbConnect.query("INSERT INTO po SET ?", po, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
