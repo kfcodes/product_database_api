5b696f1b6c2dec424f83630ac0db2e10933e58bf
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-12 16:35:57 +0100

    Models to create and remove pallet items are added and imported

--- app/models/pallet/palletItem.models.js
+++ app/models/pallet/palletItem.models.js
@@ -30,24 +30,4 @@ PalletItem.remove = (id, result) => {
   });
 };
 
-PalletItem.updatePalletItemById = (id, pallet_item, result) => {
-  sql.query(
-    `UPDATE pallet_item SET 
-    WHERE item_id=${id}`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      if (res.affectedRows == 0) {
-        result({ kind: "not_found" }, null);
-        return;
-      }
-      console.log("updated pallet Item: ", { id: id, ...pallet_item });
-      result(null, { id: id, ...pallet_item });
-    }
-  );
-};
-
 module.exports = PalletItem;
