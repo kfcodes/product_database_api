f98877496bf842bfc65281e1e741e5fbd9f88272
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-12 12:49:24 +0100

    Updated search api for finished products

--- app/models/finishedProducts/finishedProduct.models.js
+++ app/models/finishedProducts/finishedProduct.models.js
@@ -12,9 +12,34 @@ FinishedProduct.createNewFinishedProduct = (finishedProduct, result) => {
   });
 };
 
+FinishedProduct.updateFinishedProductById = (id, finishedProduct, result) => {
+  sql.query(
+    `UPDATE finishedProduct SET 
+    ${process.env.finishedProduct_7}="${finishedProduct.process.env.finishedProduct_1}",
+    ${process.env.finishedProduct_8}=${finishedProduct.process.env.finishedProduct_2},
+    ${process.env.finishedProduct_9}="${finishedProduct.process.env.finishedProduct_3}",
+    ${process.env.finishedProduct_10}="${finishedProduct.process.env.finishedProduct_4}",
+    ${process.env.finishedProduct_11}=${finishedProduct.process.env.finishedProduct_5}
+    WHERE ${process.env.finishedProduct_12}=${id}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      console.log("Updated Finished Product: ", { id: id, ...finishedProduct });
+      result(null, { id: id, ...finishedProduct });
+    }
+  );
+};
+
 FinishedProduct.findFinishedProductById = (id, result) => {
   sql.query(
-    `SELECT * FROM ${process.env.ET} WHERE ${process.env.FINISHEDPRODUCT_6}= ?`,
+    `SELECT * FROM ${process.env.ET} WHERE ${process.env.FP_6}= ?`,
     [id],
     (err, res) => {
       if (err) {
