330976b349064a06453ca6d1647877edf8bea5ec
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-12 11:44:56 +0100

    Updated the finished product models to use env variables

--- app/models/finishedProducts/finishedProduct.models.js
+++ app/models/finishedProducts/finishedProduct.models.js
@@ -2,9 +2,8 @@ const sql = require("../db/dbConnect");
 require('dotenv').config();
 const FinishedProduct = require("./finishedProduct.constructor.js")
 
-
-FinishedProduct.createNewFinishedProduct = (eol, result) => {
-  sql.query(`INSERT INTO ${process.env.ET} SET ?`, eol, (err, res) => {
+FinishedProduct.createNewFinishedProduct = (finishedProduct, result) => {
+  sql.query(`INSERT INTO ${process.env.ET} SET ?`, finishedProduct, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -13,34 +12,9 @@ FinishedProduct.createNewFinishedProduct = (eol, result) => {
   });
 };
 
-FinishedProduct.updateFinishedProductById = (id, eol, result) => {
-  sql.query(
-    `UPDATE eol SET 
-    ${process.env.EOL_7}="${eol.process.env.EOL_1}",
-    ${process.env.EOL_8}=${eol.process.env.EOL_2},
-    ${process.env.EOL_9}="${eol.process.env.EOL_3}",
-    ${process.env.EOL_10}="${eol.process.env.EOL_4}",
-    ${process.env.EOL_11}=${eol.process.env.EOL_5}
-    WHERE ${process.env.EOL_12}=${id}`,
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-      if (res.affectedRows == 0) {
-        result({ kind: "not_found" }, null);
-        return;
-      }
-      console.log("updated eol: ", { id: id, ...eol });
-      result(null, { id: id, ...eol });
-    }
-  );
-};
-
 FinishedProduct.findFinishedProductById = (id, result) => {
   sql.query(
-    `SELECT * FROM ${process.env.ET} WHERE ${process.env.EOL_6}= ?`,
+    `SELECT * FROM ${process.env.ET} WHERE ${process.env.FINISHEDPRODUCT_6}= ?`,
     [id],
     (err, res) => {
       if (err) {
@@ -49,7 +23,7 @@ FinishedProduct.findFinishedProductById = (id, result) => {
         return;
       }
       if (res.length) {
-        console.log("found FinishedProduct details: ", res);
+        console.log("found Finished Product with details: ", res);
         result(null, res);
         return;
       }
