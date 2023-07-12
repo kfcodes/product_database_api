5a66504de906ada6ca296005dc81e3dd2028a9c6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-12 11:25:40 +0100

    Create Po model uses .env variable for table name

--- app/models/po/po.models.js
+++ app/models/po/po.models.js
@@ -2,7 +2,7 @@ const dbConnect = require("../db/dbConnect.js");
 const Po = require("./po.constructor.js")
 
 Po.createNewPo = async (po, result) => {
-  dbConnect.query("INSERT INTO po SET ?", po, (err, res) => {
+  dbConnect.query(`INSERT INTO ${process.env.P} SET ?`, po, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
