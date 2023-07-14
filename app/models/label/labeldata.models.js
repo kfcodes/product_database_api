3230fb1e3d8642691cd48c98b74f18d52ebfb410
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-14 16:07:30 +0100

    Added model to get the data for product label

--- app/models/label/labeldata.models.js
+++ app/models/label/labeldata.models.js
@@ -1,7 +1,6 @@
 const sql = require("../db/dbConnect.js");
 
 exports.getPalletLabelData = (id, result) => {
-  console.log(`Printed with: ${id}`);
   sql.query(
     `SELECT * FROM ${process.env.Z_1} WHERE ${process.env.Z_2}= ${id} ${process.env.Z_3};`,
     (err, res) => {
@@ -19,3 +18,20 @@ exports.getPalletLabelData = (id, result) => {
   );
 };
 
+exports.printBoxLabel = (id, result) => {
+  sql.query(
+    `SELECT * FROM ${process.env.Z_4} WHERE ${process.env.Z_5}= ${id}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      if (res.length) {
+        result(null, res);
+        return;
+      }
+      result({ kind: "not_found" }, null);
+    }
+  );
+};
