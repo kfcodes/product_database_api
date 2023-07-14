1181a576cafc497c288d185f168cdb80f76769f1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-14 15:02:32 +0100

    Created API model to use environment variables to retrieve data from DB

--- app/models/productionSchedule/productionSchedule.models.js
+++ app/models/productionSchedule/productionSchedule.models.js
@@ -1,5 +1,19 @@
 const sql = require("../db/dbConnect.js");
 
+exports.getCurrentProduction = (result) => {
+  sql.query(
+    `SELECT * FROM ${process.env.PVV} where ${process.env.tt} IS NOT NULL ORDER by ${process.env.oo} ${process.env.order}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    }
+  );
+};
+
 exports.getAllProduction = (result) => {
   sql.query(`SELECT * FROM ${process.env.PV}`, (err, res) => {
     if (err) {
