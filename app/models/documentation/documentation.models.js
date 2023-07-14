a161c5d67259ae9ab4704a744ca72ea5c676d3a0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-14 12:07:17 +0100

    Created API models to get all products from perticular brand

--- app/models/documentation/documentation.models.js
+++ app/models/documentation/documentation.models.js
@@ -11,3 +11,17 @@ exports.getAllBrands = (result) => {
   });
 };
 
+exports.productsFromBrand = (Id, result) => {
+  sql.query(
+    `SELECT * FROM ${process.env.p} WHERE ${process.env.FP_1} LIKE "${Id}%"`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    }
+  );
+};
+
