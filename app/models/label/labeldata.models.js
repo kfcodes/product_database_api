20f8f294c7286a640c35f8f63750b95c277a04b8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-14 16:03:16 +0100

    Added label model to get pallet label information

---
+++ app/models/label/labeldata.models.js
@@ -0,0 +1,21 @@
+const sql = require("../db/dbConnect.js");
+
+exports.getPalletLabelData = (id, result) => {
+  console.log(`Printed with: ${id}`);
+  sql.query(
+    `SELECT * FROM ${process.env.Z_1} WHERE ${process.env.Z_2}= ${id} ${process.env.Z_3};`,
+    (err, res) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+      if (res.length) {
+        result(null, res);
+        return;
+      }
+      result(null);
+      return;
+    }
+  );
+};
+
