5563059f37ed0eb3cd49b7f383d1bb52a6ef2e2d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-17 12:20:45 +0100

    Created model to retrieve all products in the DB

--- app/models/finishedProducts/productDB.models.js
+++ app/models/finishedProducts/productDB.models.js
@@ -1,4 +1,18 @@
 const sql = require("../db/dbConnect");
 require('dotenv').config();
 
+exports.getAll = (result) => {
+  sql.query(
+    `${process.env.FEW}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      console.log("products: ", res);
+      result(null, res);
+    }
+  );
+};
 
