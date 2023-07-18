10083e9d5c574c690df12c9eeccdbb8fb0f2eafd
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-18 13:58:17 +0100

    Added route to update pallet item

--- app/controllers/pallet/palletItem.controllers.js
+++ app/controllers/pallet/palletItem.controllers.js
@@ -58,3 +58,25 @@ exports.createPalletItem = (req, res) => {
   });
 };
 
+exports.updatePalletItem = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty!" });
+  }
+  PalletItem.updatePalletItemById(
+    req.params.id,
+    new PalletItem(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Pallet item with id ${req.params.id} Not found`,
+          });
+        } else {
+          res.status(500).send({
+            message: `Error updating Pallet item req.params.id`,
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -5,28 +5,42 @@ const upload = require("../controllers/fileUpload/multerPdf.js");
 const ProductDB = require("../controllers/production/dbProduct.controllers");
 const Documentation = require("../controllers/production/productionDocumentation.controllers");
 const Ps = require("../controllers/production/productionSchedule.controllers");
+const PalletItem = require("../controllers/pallet/palletItem.controllers");
 
 // PRODUCT DB ROUTES
-// Retrieve all product
+// RETRIEVE ALL PRODUCT
 router.get("/products", ProductDB.findAll);
-// Retrieve all products
+// RETRIEVE ALL PRODUCTS
 router.get("/finished_products", ProductDB.findFinishedProducts);
-// Retrieve a single product with productId
+// RETRIEVE A SINGLE PRODUCT WITH PRODUCTID
 router.get("/products/:productId", ProductDB.findOne);
 
-
 // PRODUCTION DOCUMENTATION ROUTES
-// Retrieve all brands
+// RETRIEVE ALL BRANDS
 router.get("/brands", Documentation.findAllBrands);
-// Retrieve all products from specific brand
+// RETRIEVE ALL PRODUCTS FROM SPECIFIC BRAND
 router.get("/brandproducts/:id", Documentation.productsByBrand);
-// Retrieve a single product with productId
+// RETRIEVE A SINGLE PRODUCT WITH PRODUCTID
 router.get("/components/:id", Documentation.productComponents);
 
-// Get current Production
+// PRODUCTION SCHEDULE
+// CURRENT PRODUCTION
 router.get("/mps", Ps.findCurrentProduction);
-// Get All Production
+// ALL PRODUCTION
 router.get("/full_mps", Ps.findAllProduction);
+
+// PALLET ITEM ROUTES
+// GET ALL PALLET ITEMS IN THE DB
+router.get("/pallet_items", PalletItem.findAllPalletItems);
+// GET ALL PALLET ITEMS FOR A SINGLE PALLET
+router.get("/pallet_items/:id", PalletItem.findPalletItemsForPallet);
+// CREATE A NEW PALLET ITEM
+router.post("/pallet_item/:id", PalletItem.createPalletItem);
+// UPDATE A PALLET ITEM
+router.put("/pallet_item/:id", PalletItem.updatePalletItem);
+// DELETE A PALLET ITEM
+router.delete("/pallet_item/:id", PalletItem.deletePalletItem);
+
 
 // Retrieve a single product with productId
 router.get("/pallet/:palletid", products.findPallet);
@@ -37,10 +51,6 @@ router.get("/all_pallets", products.findAllPallets);
 // Get All pallets
 router.get("/pallet_data", products.findPalletData);
 router.get("/check_sheet", products.printCheckSheet);
-// Get All pallets
-router.get("/pallet_items", products.findAllPalletItems);
-// Get All pallets
-router.get("/pallet_items/:palletid", products.findPalletItemsForPallet);
 // Retrieve a single product with productId
 router.get("/pallets/:brand_prefix", products.findAllPalletsfromBrand);
 // Print the label for a single pallet
@@ -52,8 +62,6 @@ router.get("/latest_pallet_data", products.latestPalletData);
 
 // Create a new pallet
 router.post("/pallet", products.createPallet);
-// Create a new pallet item
-router.post("/pallet_item/:palletid", products.createPalletItem);
 // Print the label for a single pallet
 router.post("/box_label/:eolid", products.printBoxLabel);
 // Create EOL data
@@ -67,14 +75,10 @@ router.post("/upload_data", upload.array("files", 10), products.uploadData);
 router.post("/print_pdf/:pdfname", products.printPdf);
 router.post("/print_blank_labels", products.printBlankLabels);
 
-// update a pallet item with the id of that pallet item
-router.put("/pallet_item/:itemid", products.updatePalletItem);
 // Update a pallet with pallet id
 router.put("/pallet/:palletid", products.update);
 // Update FinishedProduct by ID
 router.put("/eol/:eolid", products.updateFinishedProduct);
 
-// Delete a brand with brandId
-router.delete("/pallet_item/:itemid", products.deletePalletItem);
 
 module.exports = router;
