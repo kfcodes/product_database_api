aef2a4b2cbe1dc943d7477db5f5dbd0e30cab45a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-18 10:15:58 +0100

    Added controller for retrieving products from the DB

---
+++ app/controllers/production/dbProduct.controllers.js
@@ -0,0 +1,14 @@
+const ProductDB = require("../../models/finishedProducts/productDB.models.js");
+
+// Retrieve all products from the database.
+exports.findAll = (req, res) => {
+  ProductDB.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving products.",
+      });
+    else res.send(data);
+  });
+};
+
