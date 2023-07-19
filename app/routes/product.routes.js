2ff69e1490f252cfe3c8533c2f508003af1cad3d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-19 12:36:09 +0100

    Modified routes folder to reflect module changes

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -8,6 +8,14 @@ const Ps = require("../controllers/production/productionSchedule.controllers");
 const PalletItem = require("../controllers/pallet/palletItem.controllers");
 const Po = require("../controllers/po/po.controllers");
 const Pallet = require("../controllers/pallet/pallet.controllers");
+const PalletLabel = require("../controllers/label/palletLabel.controllers");
+const BoxLabel = require("../controllers/label/boxLabel.controllers");
+const Checksheet = require("../controllers/checksheet/checksheet.controllers");
+const FileUpload = require("../controllers/fileUpload/fileUpload.controllers")
+const PdfLabel = require("../controllers/label/pdfLabel.controllers")
+const BlankLabel = require("../controllers/label/blankLabel.controllers")
+
+// const { boxLabel } = require("../controllers/label/oldlabels/box_label.js");
 
 // PRODUCT DB ROUTES
 // RETRIEVE ALL PRODUCT
@@ -58,33 +66,39 @@ router.put("/pallet/:id", Pallet.update);
 // DELETE A PALLET
 // router.delete("/pallet/:id", Pallet.deletePallet);
 
+// LABEL ROUTES
+// PRINT PALLET LABEL
+router.get("/label/:id", PalletLabel.printPalletLabel);
+// PRINT PRODUCT LABEL
+router.post("/box_label/:id", BoxLabel.printBoxLabel);
+// PRINT BLANK LABELS
+router.post("/print_blank_labels", BlankLabel.printBlankLabels);
+// PRINT LABELS FROM PDF FILES
+router.post("/print_pdf/:id", PdfLabel.printPdf);
+
+// PRINT CHECK SHEET
+router.get("/check_sheet", Checksheet.printCheckSheet);
+
+router.post("/upload_pdf", upload.array("files", 10), FileUpload.uploadPdf);
+router.post("/upload_data", upload.array("files", 10), FileUpload.uploadData);
+
 // Get All pallets
 router.get("/pallets", products.findRecentPallets);
 // Get All pallets
 router.get("/pallet_data", products.findPalletData);
-router.get("/check_sheet", products.printCheckSheet);
 // Retrieve a single product with productId
-router.get("/pallets/:brand_prefix", products.findAllPalletsfromBrand);
-// Print the label for a single pallet
-router.get("/label/:palletid", products.printPalletLabel);
+router.get("/pallets/:brand", products.findAllPalletsfromBrand);
 // Get single EOL data
 router.get("/eol/:eolid", products.findFinishedProduct);
 // Get All pallets
 router.get("/latest_pallet_data", products.latestPalletData);
 
-// Print the label for a single pallet
-router.post("/box_label/:eolid", products.printBoxLabel);
 // Create EOL data
 router.post("/eol", products.createFinishedProduct);
 // Dump Sql Pallet data
 router.post("/dump/:palletId", products.dumpSqlData);
-router.post("/upload_pdf", upload.array("files", 10), products.uploadPdf);
-router.post("/upload_data", upload.array("files", 10), products.uploadData);
-router.post("/print_pdf/:pdfname", products.printPdf);
-router.post("/print_blank_labels", products.printBlankLabels);
 
 // Update FinishedProduct by ID
 router.put("/eol/:eolid", products.updateFinishedProduct);
-
 
 module.exports = router;
