aadeab6b94326f66db0eb5c041effb85bfbb1737
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-19 11:17:00 +0100

    Created function for controlling the printing of box labels

---
+++ app/controllers/label/boxLabel.controllers.js
@@ -0,0 +1,18 @@
+const Label = require("../../models/label/labeldata.models");
+
+exports.printBoxLabel = (req, res) => {
+  Label.boxLabel(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Can't find Data for ${req.params.id}`,
+        });
+      } else {
+        res.status(500).send({
+          message: `Error retrieving data for ${req.params.id}`,
+        });
+      }
+    }
+    else boxLabel.boxLabel(data, req.body.batch, req.body.label_quantity);
+  });
+};
