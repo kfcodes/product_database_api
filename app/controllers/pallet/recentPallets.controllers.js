63650b811272ffeb8d5fec841535df39f0831bb3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-20 12:20:13 +0100

    Created controller to get pallet list from DB

--- app/controllers/pallet/recentPallets.controllers.js
+++ app/controllers/pallet/recentPallets.controllers.js
@@ -1,6 +1,5 @@
 const PalletList = require("../../models/palletList/palletList.models")
 
-// Retrieve all products from the database.
 exports.findRecentPallets = (req, res) => {
   PalletList.getRecentPallets((err, data) => {
     if (err)
@@ -9,5 +8,17 @@ exports.findRecentPallets = (req, res) => {
           err.message || "Some error occurred while retrieving products.",
       });
     else res.send(data);
+  });
+};
+
+exports.findPalletData = async(req, res) => {
+  PalletList.getPalletListData((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving products.",
+      });
+    else 
+    res.send(data);
   });
 };
