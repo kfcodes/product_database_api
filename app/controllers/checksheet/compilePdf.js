ffa0c80f0f597acfa608e76aef5c95d424ef393d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-21 12:56:52 +0100

    Corrected the paths for retrieving the template files for the check sheet

--- app/controllers/checksheet/compilePdf.js
+++ app/controllers/checksheet/compilePdf.js
@@ -2,14 +2,14 @@ const fs = require("fs-extra");
 const hbs = require("handlebars");
 const path = require("path");
 
-async function compile(templateName, data) {
+async function Compile(templateName, data) {
   const filePath = path.join(
     process.cwd(),
-    "/app/pdf/templates",
+    "./app/controllers/checksheet/templates",
     `${templateName}.hbs`
   );
   const html = await fs.readFile(filePath, "utf-8");
   return hbs.compile(html)(data);
 };
 
-module.exports = compile;
+module.exports = Compile;
