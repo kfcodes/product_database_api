ac3b3bf8f7616332eedcc308a421e6ef8c1b64d1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-21 16:04:17 +0100

    Updated print check sheet function to the more modular design

--- app/controllers/print/printCheckSheet.js
+++ app/controllers/print/printCheckSheet.js
@@ -1,20 +1,13 @@
-const { exec } = require("child_process");
+const PrintFunction = require("./printFunction");
 
-const printPdf = async function() {
-  await exec(
-    "lpr -P HP_ColorLaserJet_MFP_M278-M281 /home/m/checkSheet.pdf",
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-    }
-  );
- console.log("The pallet list was Printed");
+const printer = process.env.A4Printer;
+const settings = process.env.CheckSheetSettings;
+
+module.exports.printCheckSheet = (file) => {
+  return new Promise((resolve, reject) => {
+    PrintFunction(printer, settings, file).then((res) => {
+      console.log(res);
+      resolve(res);
+    });
+  });
 };
-
-module.exports.PrintPdf = printPdf;
