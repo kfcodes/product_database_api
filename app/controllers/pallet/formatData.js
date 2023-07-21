ff541f7609e3135f48d9125ecf9964a2de0eff5c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-21 15:07:28 +0100

    Created module to remove duplicate values from pallet information

---
+++ app/controllers/pallet/formatData.js
@@ -0,0 +1,17 @@
+const RemoveDuplicatValues = (pallets) => {
+  return new Promise((resolve, reject) => {
+    let previousPallet = 0;
+    pallets.forEach((item) => {
+      if (item.PALLET == previousPallet) {
+        item.WEIGHT = null;
+        item.DIMENSIONS = null;
+        item.PALLET = null;
+      } else {
+        previousPallet = item.PALLET;
+      }
+    });
+    resolve(data);
+  });
+};
+
+module.exports = RemoveDuplicatValues;
