a63e592d996d2cdf7421a959565206cc5397d214
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-21 12:48:50 +0100

    Imported function to print the Check Sheet to pdf

--- app/controllers/checksheet/checksheet.controllers.js
+++ app/controllers/checksheet/checksheet.controllers.js
@@ -1,10 +1,12 @@
 const FormatData = require("./formatPalletData");
 const getPalletData = require("./getPalletData");
+const CreatePDF = require("./pdf");
 
 exports.printCheckSheet = async (req, res) => {
   getPalletData().then((palletdata) => {
     FormatData(palletdata).then((checksheetData) => {
-      res.send(checksheetData);
+      CreatePDF.CreatePdf(checksheetData);
+      res.send("The check sheet is being printed");
     });
   });
 };
