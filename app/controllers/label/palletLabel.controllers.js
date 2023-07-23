78f26e00dc781fde27921de0978204f59b8e2ce9
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-23 21:19:45 +0100

    Modified the pallet label controller for clarity

--- app/controllers/label/palletLabel.controllers.js
+++ app/controllers/label/palletLabel.controllers.js
@@ -1,20 +1,18 @@
-const Label = require("../../models/label/labeldata.models");
-const labels = require("../../controllers/label/oldlabels/actual_pallet_label_with_data");
+const LabelData = require("../../models/label/labeldata.models");
 
 exports.printPalletLabel = (req, res) => {
-  Label.getPalletLabelData(req.params.id, (err, data) => {
+  LabelData.getPalletLabelData(req.params.id, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Can't find Pallet ${req.params.id}`,
+          message: `Can't find data for ${req.params.id}`,
         });
       } else {
         res.status(500).send({
-          message: `Error finding Pallet ${req.params.id}`,
+          message: `Error finding data for ${req.params.id}`,
         });
       }
     } else 
       labels.CreateLabel(data);
-    console.log(`Printing label`);
   });
 };
