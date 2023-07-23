805747671c1cac1f3e46a342cb63b98d57fefe06
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-23 21:38:27 +0100

    Impoted module to split and print pdf files

--- app/controllers/label/pdfLabel.controllers.js
+++ app/controllers/label/pdfLabel.controllers.js
@@ -1,13 +1,15 @@
+const printPdf = require("./pdfSplit");
+
 exports.printPdf = (req, res) => {
   printPdf.PrintPdf(req.params.id, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `There is no PDF ${req.params.id}`,
+          message: `There is no ${req.params.id}`,
         });
       } else {
         res.status(500).send({
-          message: `Error retrieving PDF ${req.params.id}`,
+          message: `Error retrieving ${req.params.id}`,
         });
       }
     } else res.send({ message: `Printing ${req.params.id}` });
