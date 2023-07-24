7d5c5fb9a578741b33407a689172b5a23d61c924
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-24 15:32:40 +0100

    Removed unused code for printing the pallet label

--- app/controllers/label/jsFiles/pallet/index.js
+++ app/controllers/label/jsFiles/pallet/index.js
@@ -1,6 +1,4 @@
-const fs = require("fs");
-const sql = require("../models/db");
-const { exec } = require("child_process");
+const newLoop = require("./palletProductsLoop")
 
 module.exports.CreateLabel = async function (results) {
    newLoop(results);
@@ -9,15 +7,6 @@ module.exports.CreateLabel = async function (results) {
 
 function processFile() {
   // printPalletLabel();
-  console.log("Printed");
-}
-
-function writeToLabel(pallet) {
-  fs.appendFile("product_pallet.zpl", pallet, (err) => {
-    if (err) console.log(err);
-    else {
-      console.log("File written successfully\n");
-    }
-  });
+  console.log("The label is ready to print");
 }
 
