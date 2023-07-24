5cde76940edd8c599704c2d1aac20d6e941d8c26
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-24 12:37:33 +0100

    The print pdf now appends pdf to the end of the filename

--- app/controllers/print/printLabelsFromPDF.js
+++ app/controllers/print/printLabelsFromPDF.js
@@ -3,9 +3,11 @@ const PrintFunction = require("./printFunction");
 const printer = process.env.LargeLabelPrinter;
 const settings = process.env.PDFLablelSettings;
 
+
 module.exports.printPdfFile = (file) => {
+const printFile = `${file}.pdf`
   return new Promise((resolve, reject) => {
-    PrintFunction(printer, settings, file).then((res) => {
+    PrintFunction(printer, settings, printFile).then((res) => {
       resolve(res);
     });
   });
