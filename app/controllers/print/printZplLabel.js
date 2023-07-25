dd5116442b8fb6986a190a1501cdcb0851f86131
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-25 11:22:14 +0100

    removed test code from print module

--- app/controllers/print/printZplLabel.js
+++ app/controllers/print/printZplLabel.js
@@ -3,10 +3,9 @@ const PrintFunction = require("./printFunction");
 const printer = process.env.LargeLabelPrinter;
 const settings = process.env.LargeLabelPrinterSettings;
 
-module.exports.printLargeLabel = (file) => {
+module.exports.PrintLargeLabel = (file) => {
   return new Promise((resolve, reject) => {
     PrintFunction(printer, settings, file).then((res) => {
-      console.log(res);
       resolve(res);
     });
   });
