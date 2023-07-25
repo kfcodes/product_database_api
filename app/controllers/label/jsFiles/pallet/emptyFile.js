3e95cdcbd5ee860fa061fa3c3e874972a0e1b42a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-25 11:27:01 +0100

    Improved resolve from promise for creating file

--- app/controllers/label/jsFiles/pallet/emptyFile.js
+++ app/controllers/label/jsFiles/pallet/emptyFile.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-const emptyFile = () => {
+const CreateEmptyLabelFile = () => {
   return new Promise((resolve, reject) => {
     fs.writeFile(`${process.env.palletFileName}`, "", function () {
-      resolve(console.log("The File was emptied"));
+      resolve(console.log("Created File"));
     });
   });
 };
 
-module.exports = emptyFile;
+module.exports = CreateEmptyLabelFile;
