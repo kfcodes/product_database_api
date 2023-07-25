fcec411180969f778f948ffabed1c446cb49fcba
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-25 10:06:06 +0100

    Updated the write to file function to take in the data to be written and return promise

--- app/controllers/label/jsFiles/pallet/writeToLabel.js
+++ app/controllers/label/jsFiles/pallet/writeToLabel.js
@@ -1,12 +1,14 @@
+const fs = require("fs");
+
 const writeToLabel = (data) => {
   return new Promise((resolve, reject) => {
-  fs.appendFile(`${process.env.palletFileName}`, data, (err) => {
-    if (err) console.log(err);
-    else {
-      resolve("File written successfully\n");
-    }
+    fs.appendFile(`${process.env.palletFileName}`, data, (err) => {
+      if (err) console.log(err);
+      else {
+        resolve(console.log("File written successfully\n"));
+      }
+    });
   });
-  });
-}
+};
 
 module.exports = writeToLabel;
