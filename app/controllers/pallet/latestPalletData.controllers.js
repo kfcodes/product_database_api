ca896fe360c2c13f70840c7eaeaf91dea8d97d84
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-27 15:58:27 +0100

    Created Function to retrieve all the pallet products from the DB

--- app/controllers/pallet/latestPalletData.controllers.js
+++ app/controllers/pallet/latestPalletData.controllers.js
@@ -13,3 +13,29 @@ const latestPallets = () => {
     });
   });
 };
+
+const latestPalletProducts = (id) => {
+  return new Promise((resolve, reject) => {
+    PalletList.getLatestPalletProducts(id, (err, data) => {
+      const products_array = [];
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving products.",
+        });
+      else
+        for (let j = 0; j < data.length; j++) {
+          const pallet_product = {
+            ID: data[j].pallet_item_product_id,
+            LOT: data[j].lot,
+            BBE: data[j].bbe,
+            BATCH: data[j].batch,
+            QTY: data[j].quantity,
+            DESCRIPTION: data[j].product_description,
+          };
+          products_array.push(pallet_product);
+        }
+      resolve(products_array);
+    });
+  });
+};
