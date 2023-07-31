db9f946e15a89d9d74170871c2c072cfa2ba06dc
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-07-31 11:44:47 +0100

    Improved formatting for formatting pallet Data

--- app/modules/palletData/palletData.js
+++ app/modules/palletData/palletData.js
@@ -1,30 +1,30 @@
 exports.formatPalletProducts = (products) => {
   return new Promise((resolve, reject) => {
-      const palletProducts = [];
-        for (let j = 0; j < products.length; j++) {
-          const product = {
-            ID: products[j].pallet_item_product_id,
-            LOT: products[j].lot,
-            BBE: products[j].bbe,
-            BATCH: products[j].batch,
-            QTY: products[j].quantity,
-            DESCRIPTION: products[j].product_description,
-          };
-          palletProducts.push(product);
-        }
-      resolve(palletProducts);
+    const palletProducts = [];
+    for (let j = 0; j < products.length; j++) {
+      const product = {
+        ID: products[j].pallet_item_product_id,
+        LOT: products[j].lot,
+        BBE: products[j].bbe,
+        BATCH: products[j].batch,
+        QTY: products[j].quantity,
+        DESCRIPTION: products[j].product_description,
+      };
+      palletProducts.push(product);
+    }
+    resolve(palletProducts);
   });
 };
 
 exports.formatPalletData = async (pallet, products) => {
   return new Promise((resolve, reject) => {
-        const pallet_data = {
-          height: pallet.height,
-          weight: pallet.weight,
-          pallet: pallet.pallet_id,
-          type: pallet.pallet_name,
-        };
-        pallet_data.products = products;
+    const pallet_data = {
+      height: pallet.height,
+      weight: pallet.weight,
+      pallet: pallet.pallet_id,
+      type: pallet.pallet_name,
+    };
+    pallet_data.products = products;
     resolve(pallet_data);
-})
+  });
 };
