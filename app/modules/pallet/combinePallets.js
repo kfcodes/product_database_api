9977fef5030a0a471d5e4b052df3952ce28406c1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-03 15:09:21 +0100

    Improved the formatting for the pallet combine function

--- app/modules/pallet/combinePallets.js
+++ app/modules/pallet/combinePallets.js
@@ -5,14 +5,14 @@ const combinePallets = (pallets) => {
     for (let i = 0; i < pallets.length; i++) {
       if (pallets[i] > combinedId) {
         combinedId = pallets[i];
-        if (i == pallets.length - 1) {
-          palletString += `"${pallets[i]}"`;
-        } else {
-          palletString += `"${pallets[i]}", `;
-        }
+      }
+      if (i == pallets.length - 1) {
+        palletString += `"${pallets[i]}"`;
+      } else {
+        palletString += `"${pallets[i]}", `;
       }
     }
-    resolve({ id: combinedId, pallets: `( ${palletString} )` });
+    resolve({ id: combinedId, pallets: `(${palletString})` });
   });
 };
 
