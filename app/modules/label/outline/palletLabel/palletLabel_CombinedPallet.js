2e7d32cab393fb60779c11a3ae616dcb0853f63f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-04 12:24:12 +0100

    Improved the fromat for combined pallet label structure

--- app/modules/label/outline/palletLabel/palletLabel_CombinedPallet.js
+++ app/modules/label/outline/palletLabel/palletLabel_CombinedPallet.js
@@ -3,24 +3,22 @@ const GetCombinedPalletLabelStructure = () => {
     let labelStructure = `
       ^XA^CFD
       ^FWR
-      ^LH2,20
+      ^LH20,20
+      ^FO630,220^A0,100^FDCOMBINED PALLET^FS
 
-      ^FO650,250^A0,90^FDCOMBINED PALLET^FS
+      ^FO520,480^A0,60^FDPALLETS^FS
+      ^FO250,10^GB360,1170,5^FS
 
-      ^FO320,10^GB300,1170,5^FS
-      ^FO550,500^A0,40^FDPALLETS^FS
-
-      ^FO90,20^A0,55^FDGROSS:^FS
-      ^FO30,20^A0,55^FDWEIGHT:^FS
+      ^FO90,30^A0,55^FDGROSS^FS
+      ^FO30,30^A0,55^FDWEIGHT^FS
       ^FO65,450^A0,60^FDKG^FS
       ^FO10,10^GB180,530,5^FS
 
       ^FO100,620^A0,60^FDGROSS^FS
-      ^FO30,620^A0,60^FDHEIGHT:^FS
+      ^FO30,620^A0,60^FDHEIGHT^FS
       ^FO65,1060^A0,60^FDCM^FS
       ^FO10,580^GB180,580,5^FS
       `;
-
     resolve(labelStructure);
   });
 };
