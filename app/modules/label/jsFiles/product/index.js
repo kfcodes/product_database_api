deecda859b15a23088185eaf61a29f4382e02ce9
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-28 12:50:08 +0100

    The product index file takes the values for the components and combines and prints the labels

--- app/modules/label/jsFiles/product/index.js
+++ app/modules/label/jsFiles/product/index.js
@@ -1,5 +1,17 @@
-module.exports.CreateProductLabel = async function (data) {
-  writeToLabel("outline")
-    .then(writeToLabel("data").then(writeToLabel("end")))
-    .catch((err) => console.log(err));
+const CreateEmptyLabelFile = require("../../../../modules/file/createEmptyFile");
+const writeToLabel = require("../../../file/writeToLabel");
+const productLabel = require("../../outline/boxLabel/defaultProductLabel");
+const labelData = require("./defaultProductLabelData");
+const { PrintLargeLabel } = require("../../../../modules/print/printFile");
+
+module.exports.PrintBoxLabel = async function (data) {
+  CreateEmptyLabelFile().then(() => {
+    productLabel().then((outline) => {
+      writeToLabel(outline).then(() => {
+        labelData(data).then((file) => {
+          PrintLargeLabel(file);
+        });
+      });
+    });
+  });
 };
