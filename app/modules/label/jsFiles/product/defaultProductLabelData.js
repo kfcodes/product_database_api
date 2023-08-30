5241658ed831dc7ed89aa7dc7eb1a1de423dd3bf
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-30 15:16:14 +0100

    Modified the default product label to use stored font and font size

--- app/modules/label/jsFiles/product/defaultProductLabelData.js
+++ app/modules/label/jsFiles/product/defaultProductLabelData.js
@@ -1,19 +1,27 @@
 const LabelData = (data, qty) => {
   return new Promise((resolve, reject) => {
   let label = `
-^PQ${qty},10,1,Y
-^FO710,200^A0,70^FD${data.brand}^FS
-^FX Product details
-^FO650,250^AC,40^FD${data.sku}^FS
-^FO600,250^AC,40^FD${data.product}^FS
-^FO550,250^AC,40^FD${data.flavour}^FS
-^FO500,250^AC,40^FD${data.quantity_per_box}^FS
-^FO450,250^AC,40^FD${data.weight}^FS
-^FO400,250^AC,40^FD${data.eol_lot}^FS
-^FO350,250^AC,40^FD${data.eol_bbe}^FS
-^FO300,250^AC,40^FD${data.eol_batch}^FS
+^FS
+^FO550,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.flavour}^FS
+^FO500,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.quantity_per_box}^FS
+^FO450,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.weight}^FS
+^FO400,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.eol_lot}^FS
+^FO350,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.eol_bbe}^FS
+^FO300,250
+^A@90,50,50,E:ARIALB.TTF
+^FD${data.eol_batch}^FS
 ^BY8
-^FO120,200^BER,150,Y,N,N,N,N^FD${data.box_barcode}^FS
+^FO70,200^BER,210,Y,N,N,N,N^FD${data.box_barcode}^FS
 ^XZ
 `;
           resolve(label);
