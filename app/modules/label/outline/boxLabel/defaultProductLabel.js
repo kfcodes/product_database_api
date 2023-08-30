b5b5d884b6d59cbe9d033f44d35e9331301bc1ca
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-30 15:21:27 +0100

    Update the size of the font in the label structure

--- app/modules/label/outline/boxLabel/defaultProductLabel.js
+++ app/modules/label/outline/boxLabel/defaultProductLabel.js
@@ -1,23 +1,18 @@
 const productLabel = () => {
   return new Promise((resolve, reject) => {
     let product = `
-      ^FX BOX LABELS WITH PRODUCT INFORMATION
-      ^XA^CFD
-      ^FWR
-      ^LH2,20
-
-      ^FX The product details that will be stored in the DB
-      ^FO650,110^A0,40^FDSKU:^FS
-      ^FO600,20^A0,40^FDPRODUCT:^FS
-      ^FO550,20^A0,40^FDFLAVOUR:^FS
-      ^FO500,110^A0,40^FDQTY:^FS
-      ^FO450,100^A0,40^FDSIZE:^FS
-
-      ^FX The details for each product that will be entered by the user
-      ^FO400,110^A0,40^FDLOT:^FS
-      ^FO350,110^A0,40^FDBBE:^FS
-      ^FO300,60^A0,40^FDBATCH:^FS
-      `;
+^XA^CFD
+^FWR
+^LH2,20
+^FO650,110^A0,50^FDSKU:^FS
+^FO600,20^A0,50^FDPRODUCT:^FS
+^FO550,20^A0,50^FDFLAVOUR:^FS
+^FO500,110^A0,50^FDQTY:^FS
+^FO450,100^A0,50^FDSIZE:^FS
+^FO400,110^A0,50^FDLOT:^FS
+^FO350,110^A0,50^FDBBE:^FS
+^FO300,60^A0,50^FDBATCH:^FS
+`;
     resolve(product);
   });
 };
