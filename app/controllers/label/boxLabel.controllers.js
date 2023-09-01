ee0f4636346210115455aaaad81b790ecedcaf47
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-01 11:19:09 +0100

    Added controller for retrieving box label form the DB

--- app/controllers/label/boxLabel.controllers.js
+++ app/controllers/label/boxLabel.controllers.js
@@ -1,6 +1,22 @@
 const Label = require("../../models/label/labeldata.models");
 const { PrintBoxLabel }  = require("../../modules/label/jsFiles/product/index");
 
+exports.getBoxLabel = (req, res) => {
+  Label.boxLabel(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Can't find Data for ${req.params.id}`,
+        });
+      } else {
+        res.status(500).send({
+          message: `Error retrieving data for ${req.params.id}`,
+        });
+      }
+    } else 
+      res.send({message: "This is the label data"})
+    })};
+
 exports.printBoxLabel = (req, res) => {
   Label.boxLabel(req.params.id, (err, data) => {
     if (err) {
