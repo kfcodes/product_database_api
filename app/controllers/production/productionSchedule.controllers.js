5a4a8d2f92a5bcf2afcc92b2c15845f7835623d2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-28 14:33:27 +0100

    Added controller to get individual production Record information

--- app/controllers/production/productionSchedule.controllers.js
+++ app/controllers/production/productionSchedule.controllers.js
@@ -1,7 +1,19 @@
 const ProductionSchedule = require("../../models/productionSchedule/productionSchedule.models.js");
 
-exports.findAllProduction = (req, res) => {
-  ProductionSchedule.getAllProduction((err, data) => {
+exports.findAllProductionById = (req, res) => {
+  ProductionSchedule.getProductionInfoById(req.params.id,(err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving production data.",
+      });
+    else res.send(data);
+  });
+};
+
+exports.findProductionRecordById = (req, res) => {
+  ProductionSchedule.getCurrentProduction((err, data) => {
     if (err)
       res.status(500).send({
         message:
