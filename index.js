a9fd922fd8266762ede7d1f2d5ca266c84007572
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-14 23:16:37 +0000

    Removed unused comments from index module

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// Fixed imports and routes
-  express = require("express"),
-  app = express(),
-  cors = require("cors"),
-  bodyParser = require("body-parser");
+express = require("express"),
+app = express(),
+cors = require("cors"),
+bodyParser = require("body-parser");
 
-// make server object that contain port property and the value for our server.
 var server = {
   port: 4040,
 };
 
-// use the modules
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// routers
 const productsRouter = require("./app/routes/application.routes.js");
-// use router
 app.use("/", productsRouter);
 
-// starting the server
 app.listen(server.port, () =>
   console.log(`Server started, listening port: ${server.port}`)
 );
