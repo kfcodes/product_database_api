90305f8208660436f898d5b870928d85fb7d34cf
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-14 12:23:05 +0000

    Updated the combined pallet label controller to send the pallet data to a remote server for printing

--- app/modules/label/jsFiles/pallet/combinedPallet.js
+++ app/modules/label/jsFiles/pallet/combinedPallet.js
@@ -3,6 +3,8 @@ const writeToLabel = require("../../../file/writeToLabel");
 const GetPalletLabelStructure = require("../../outline/palletLabel/palletLabel_CombinedPallet");
 const AddPalletData = require("./combinedPalletData");
 const { PrintLargeLabel } = require("../../../../modules/print/printFile");
+const fs = require('node:fs');
+const fetch = require("node-fetch");
 
 module.exports.PrintPalletLabel = async function (data) {
   CreateEmptyLabelFile().then(() => {
@@ -10,11 +12,21 @@ module.exports.PrintPalletLabel = async function (data) {
       writeToLabel(outline).then(() => {
         AddPalletData(data).then((palletData) => {
           writeToLabel(palletData).then((file) => {
-            console.log(file);
-            PrintLargeLabel(file);
-          });
-        });
-      });
+              const data = fs.readFileSync(file, 'utf8');
+		        const label_data = { label_info: data };
+              fetch(`http://192.168.1.26:8000/print_large_combined_label/`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(label_data),
+    }).then((result) => {
+      console.log(result);
     });
+    });
+  });
+  });
+  });
   });
 };
