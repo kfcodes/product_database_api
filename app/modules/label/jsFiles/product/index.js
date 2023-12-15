47de91a3b90bb94c4affdb605789967df02e6c42
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-15 15:35:29 +0000

    Updated the function to allow users to print the box label data from the API

--- app/modules/label/jsFiles/product/index.js
+++ app/modules/label/jsFiles/product/index.js
@@ -1,7 +1,7 @@
 const CreateEmptyLabelFile = require("../../../../modules/file/createEmptyFile");
 const writeToLabel = require("../../../file/writeToLabel");
 const productLabel = require("../../outline/boxLabel/defaultProductLabel");
-const LabelData= require("./defaultProductLabelData");
+const LabelData = require("./defaultProductLabelData");
 const { PrintLargeLabel } = require("../../../../modules/print/printFile");
 
 module.exports.PrintBoxLabel = async function (data, qty) {
@@ -10,10 +10,21 @@ module.exports.PrintBoxLabel = async function (data, qty) {
       writeToLabel(outline).then(() => {
         LabelData(data[0], qty).then((completefile) => {
           writeToLabel(completefile).then((file) => {
-          PrintLargeLabel(file);
+            const data = fs.readFileSync(file, "utf8");
+            const label_data = { label_info: data };
+            fetch(`http://192.168.1.26:8000/print_large_combined_label/`, {
+              method: "post",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(label_data),
+            }).then((result) => {
+              console.log(result);
+            });
+          });
         });
       });
     });
-  });
   });
 };
